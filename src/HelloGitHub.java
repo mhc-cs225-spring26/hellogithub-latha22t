1ea9e14b096ade70c3d6d5db0b4290b5549c2612
latha22t <clem_lover@Clems-MacBook-Air.local>
2026-02-06 12:39:18 -0500

    2nd version of hello world

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -4,6 +4,7 @@ public class HelloGitHub {
     public static void main(String[] args) {
         // TODO Write your code here
     	System.out.println("Hello, world");
+    	System.out.println("Olá, mundo");
 
     }
 
